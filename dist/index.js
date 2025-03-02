c97a93385ad9155e5c10aab01ab3f7acf448c2a6
yash lalage <yashlalage@gmail.com>
2025-03-02 23:05:49 +0530

    Add omit and generic

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,77 @@ function sumOfAge(user1, user2) {
 }
 let ans = sumOfAge({ name: "yash", age: 23 }, { name: "nt", age: 20 });
 console.log(ans);
+let userProfile = {
+    name: "yash",
+    age: 12,
+    // password:"yash" // this is not available now
+};
+let partialProf = {
+    name: "yash",
+    //we can exclude age here
+};
+const player1 = {
+    rank: 122,
+    name: "yash",
+    password: "passs@123e"
+};
+//we can not update properties of object now -> it is like Object.freez();
+player1.rank = 12;
+const player2 = {
+    name: "yash",
+    password: "yashh",
+    rank: 1443
+};
+player2.name = "lalage"; //can not do this
+let gamers = {
+    "gamer1": {
+        rank: 123,
+        id: 12,
+        name: "yash"
+    },
+    "gamer2": {
+        rank: 1234,
+        id: 13,
+        name: "bot"
+    }
+};
+let gamersRecord = {
+    "gamer1": {
+        rank: 123,
+        id: 12,
+        name: "yash"
+    },
+    "gamer2": {
+        rank: 1234,
+        id: 13,
+        name: "bot"
+    }
+};
+let empEmil = {
+    name: "yash",
+    email: "yash@gmail.com",
+    isActive: false,
+};
+let status2 = "pending";
+let status3 = "active";
+//generics
+function identity(val) {
+    return val;
+}
+let res1 = identity("yash"); // Explicit type
+let res2 = identity(12); // Explicit type
+let res3 = identity(true); // TypeScript infers `boolean`
+//multiple type parameter
+function pair(vla1, val2) {
+    return [vla1, val2];
+}
+let res4 = pair("yash", 13);
+console.log(res);
+const numberBox = { value: 20 };
+const stringBox = { value: "yash" };
+//default generic
+function defaultIdentity(val) {
+    return val;
+}
+defaultIdentity("yash");
+defaultIdentity(12);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+//Typescript untility type which supports genric.
+//Used to transform types.
 interface UserTemp {
     name: string;
     age: number;
@@ -7,7 +9,7 @@ function sumOfAge(user1: UserTemp, user2: UserTemp) {
     return user1.age + user2.age;
 }
 
-let ans = sumOfAge({name:"yash",age:23},{name:"nt",age:20});
+let ans = sumOfAge({ name: "yash", age: 23 }, { name: "nt", age: 20 });
 console.log(ans);
 
 interface User {
@@ -41,10 +43,10 @@ type player = {
     readonly password: string,
 }
 
-const player1:player = {
-    rank:122,
-    name:"yash",
-    password:"passs@123e"
+const player1: player = {
+    rank: 122,
+    name: "yash",
+    password: "passs@123e"
 }
 
 //we can not update properties of object now -> it is like Object.freez();
@@ -52,58 +54,114 @@ player1.rank = 12;
 
 //another syntax for Readonly
 type player2 = {
-    rank:number;
-    name:string;
-    password:string
+    rank: number;
+    name: string;
+    password: string
 }
 
-const player2:Readonly<player2> = {
-    name:"yash",
-    password:"yashh",
-    rank:1443
+const player2: Readonly<player2> = {
+    name: "yash",
+    password: "yashh",
+    rank: 1443
 }
 
 player2.name = "lalage";//can not do this
 
 // 4->Record
 type Gamer = {
-    rank:number;
-    id:number;
-    name:string
+    rank: number;
+    id: number;
+    name: string
 }
 
 type Gamers = {
-    [key:string]:Gamer
+    [key: string]: Gamer
 }
 
-let gamers:Gamers = {
-    "gamer1":{
-        rank:123,
-        id:12,
-        name:"yash"
+let gamers: Gamers = {
+    "gamer1": {
+        rank: 123,
+        id: 12,
+        name: "yash"
     },
-    "gamer2":{
-        rank:1234,
-        id:13,
-        name:"bot"
+    "gamer2": {
+        rank: 1234,
+        id: 13,
+        name: "bot"
     }
 }
 //insted of above approch we can do this same with records with more cleaner way
 
-type GamersRecord = Record<string,Gamer>
+type GamersRecord = Record<string, Gamer>
 
- let gamersRecord:GamersRecord = {
-    "gamer1":{
-        rank:123,
-        id:12,
-        name:"yash"
+let gamersRecord: GamersRecord = {
+    "gamer1": {
+        rank: 123,
+        id: 12,
+        name: "yash"
     },
-    "gamer2":{
-        rank:1234,
-        id:13,
-        name:"bot"
+    "gamer2": {
+        rank: 1234,
+        id: 13,
+        name: "bot"
     }
- }
+}
 
 
+//omit
+interface Employee {
+    empid: number;
+    name: string;
+    email: string;
+    isActive: boolean
+}
 
+let empEmil: Omit<Employee, 'empid'> = {
+    name: "yash",
+    email: "yash@gmail.com",
+    isActive: false,
+}
+
+//Exclude -> We can use Exclude<> with types only for interface we can use omit
+type Status2 = "active" | "inactive" | "pending";
+
+let status2: Exclude<Status2, 'pending'> = "pending";
+
+let status3: Exclude<Status2, 'pending'> = "active";
+
+//generics
+function identity<T>(val: T): T {
+    return val;
+}
+
+let res1 = identity<string>("yash"); // Explicit type
+
+let res2 = identity<number>(12); // Explicit type
+
+let res3 = identity(true);// TypeScript infers `boolean`
+
+//multiple type parameter
+function pair<T,U>(vla1:T,val2:U):[T,U]{
+    return [vla1,val2]
+}
+
+let res4 = pair<string,number>("yash",13)
+console.log(res);
+
+//generics in interface
+
+interface Box<T>{
+    value:T
+}
+
+const numberBox: Box<number> = {value:20};
+
+const stringBox: Box<string> = {value:"yash"};
+
+//default generic
+function defaultIdentity<T = string>(val:string):T{
+    return val;
+}
+
+defaultIdentity("yash");
+defaultIdentity<number>(12);
